ccbb3c72fc8726a82997b04b4b92375eb3342586
Oswaldo Garcia <oswagaru@gmail.com>
2024-04-17 16:20:53 -0600

    Update

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import screenmatch.modelos.Pelicula;
+import screenmatch.modelos.Serie;
+
+
+
+public class  Main {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDelasEvaluaciones());
+        System.out.println("Média de evaluaciones de la película: " + miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragón");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+    }
+}
---
+++ src/screenmatch/calculos/CalculadoraDeTiempo.java
@@ -0,0 +1,4 @@
+package screenmatch.calculos;
+
+public class CalculadoraDeTiempo {
+}
